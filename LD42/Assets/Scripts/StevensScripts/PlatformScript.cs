343571440eace6856c817020b4c0bbd37be6f490
Steven V <svandegrift@icloud.com>
2018-08-11 16:30:39 -0500

    Finished platform code
    The platform code is complete, 1 second after you touch a platform it will explod and fall apart.

---
+++ LD42/Assets/Scripts/StevensScripts/PlatformScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+    public GameObject Destroyed_Version;
+    public int timer;
+
+
+	public void OnCollisionEnter(Collision col)
+	{
+        if(col.gameObject.tag == "player")
+        {
+            Debug.Log("About to explode");
+            StartCoroutine(FallApart());
+        }
+	}
+
+    IEnumerator FallApart()
+    {
+        yield return new WaitForSeconds(timer);
+        Destroy(gameObject);
+        Instantiate(Destroyed_Version, transform.position, transform.rotation);
+    }
+}
